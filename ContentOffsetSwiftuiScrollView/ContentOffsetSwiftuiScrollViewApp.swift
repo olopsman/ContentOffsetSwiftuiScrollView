abd9bf32a3518a07526f6d4141353c305e516369
Paulo Orquillo <olopsman@users.noreply.github.com>
2023-02-05 14:40:05 +1300

    Initial Commit

---
+++ ContentOffsetSwiftuiScrollView/ContentOffsetSwiftuiScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContentOffsetSwiftuiScrollViewApp.swift
+//  ContentOffsetSwiftuiScrollView
+//
+//  Created by Paulo Orquillo on 5/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ContentOffsetSwiftuiScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContentOffsetSwiftuiScrollView/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ContentOffsetSwiftuiScrollView
+//
+//  Created by Paulo Orquillo on 5/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
